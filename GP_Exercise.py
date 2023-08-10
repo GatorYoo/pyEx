b4b83851a69c18293478227eaf9ad21e26907ebb
GatorYoo <d.g.yoo88@gmail.com>
2023-08-10 15:59:55 +0900

    Created a file to exercise Gaussian Process fitting with Tensorflow

---
+++ GP_Exercise.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Aug 10 15:53:02 2023
+
+@author: gator
+"""
+
+from IPython import get_ipython
+get_ipython().magic('reset -sf')
+
